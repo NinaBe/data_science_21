0266a999757458bdf5c0b3b524995e9d07f83ec0
Nina Berndorfer <nina.berndorger@googlemail.com>
2021-06-04 19:19:05 +0200

    scnd commit

--- App.R
+++ App.R
@@ -7,19 +7,29 @@ library(e1071)
 library(shiny)
 
 
-model.svm <- readRDS("titanic.svm.rds")
+titanic.svm <- readRDS("titanic.svm.rds")
 
 
 ui <- fluidPage(
+  
   titlePanel("Wer überlebt auf der Titanic?"),
+  
   sidebarLayout(
+    
     sidebarPanel(
+      
       numericInput("age", "Alter:", value = ""),
+      
       selectInput("sex", selected = NULL, "Geschlecht:", c("weiblich" = 1, "männlich" = 0)),
+      
       sliderInput("pclass", "Klasse:", min = 1, max = 3, value = 1),
-      helpText("Geben Sie das Alter, Geschlecht und die Klasse an und erfahren Sie, ob die Person auf der Titanic überlebt hätte."),
+      
+      helpText("Geben Sie Alter, Geschlecht und Klasse an und erfahren Sie, ob die Person auf der Titanic überlebt hätte."),
+      
       actionButton("action", label = "Überlebt die Person?")),
+    
     mainPanel(
+      
       tableOutput("value1")
     )
   )
@@ -28,13 +38,21 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
+  
   observeEvent(input$action, {
+    
     age <- input$age
+    
     sex <- as.numeric(input$sex)
+    
     pclass <- as.numeric(input$pclass)
-    data.frame(age, sex, pclass)
-    result <- predict(model.svm, data, probability = TRUE)
+    
+    data <- data.frame(age, sex, pclass)
+    
+    result <- predict(titanic.svm, data, probability = TRUE)
+    
     my_result <- data.frame(attr(result, "probabilities"))
+    
     output$value1 <- renderTable(my_result)
     
     }
@@ -42,3 +60,5 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
+
+
